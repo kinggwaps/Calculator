078ed393abe6be26afbac5a78b253eb1dfc5446d
kinggwaps <167969411+kinggwaps@users.noreply.github.com>
2024-05-24 13:39:53 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c;
+            if (radioButton1.Checked)
+            {
+                c = a + b;
+                label4.Text = c. ToString(); 
+            }
+            if (radioButton2.Checked)
+            {
+                c = a - b;
+                label4.Text = c.ToString();
+            }
+            if (radioButton3.Checked)
+            {
+                c = a * b;
+                label4.Text = c.ToString();
+            }
+            if (radioButton4.Checked)
+            {
+                c = a / b;
+                label4.Text = c.ToString();
+            }
+            if (radioButton5.Checked)
+            {
+                c = a % b;
+                label4.Text = c.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label4.Text = "";
+        }
+    }
+}
